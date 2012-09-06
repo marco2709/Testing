9ce0db09bebc23e9979515901f2ace7429f5801a
Marco Riquelme <marco@vincent-ntbk.deathlock.org>
2012-09-06 17:59:48 -0300

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '9abeafd897eba12ae80cab572205ff1337907b834d5647a361be64b9a02561a759f73bfa592e450bc20f7524ceb068e5d2d0d0872327f7e3c78ee0f8819107a0'
